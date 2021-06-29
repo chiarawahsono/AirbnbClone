902973aa4007ce448317df04c4996be4ac0860ba
Chiara Wahsono <chiarawahsono@gmail.com>
2021-06-29 17:08:31 +0700

    Google Maps component working

--- AirbnbClone/src/screens/SearchResultsMap/index.js
+++ AirbnbClone/src/screens/SearchResultsMap/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
 
 const SearchResultsMap = props => {
   return (
-    <View>
-      <Text>not just a component</Text>
+    <View style={{width: '100%', height: '100%'}}>
+      <MapView
+        style={{width: '100%', height: '100%'}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
