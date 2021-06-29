fa4ac486dbd59f4fa0a85a7db6e862db1027b05e
Chiara Wahsono <chiarawahsono@gmail.com>
2021-06-29 15:16:55 +0700

    SuggestionRow styles

--- AirbnbClone/src/screens/LocationSearch/index.js
+++ AirbnbClone/src/screens/LocationSearch/index.js
@@ -5,6 +5,7 @@ import searchResults from '../../../assets/data/search';
 import Entypo from 'react-native-vector-icons/Entypo';
 import {useNavigation} from '@react-navigation/native';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import SuggestionRow from './SuggestionRow';
 
 const LocationSearchScreen = props => {
   const [inputText, setInputText] = useState('');
@@ -12,17 +13,27 @@ const LocationSearchScreen = props => {
 
   return (
     <View style={styles.container}>
-      <GooglePlacesAutocomplete
-        placeholder="Search"
-        onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
-          console.log(data, details);
-        }}
-        query={{
-          key: 'AIzaSyBzHzcHRupfXyaO6g8S50dr6eWnx4IBpUI',
-          language: 'en',
-        }}
-      />
+      <View style={{height: 500}}>
+        <GooglePlacesAutocomplete
+          placeholder="Where are you going?"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          styles={{
+            textInput: styles.textInput,
+          }}
+          query={{
+            key: 'AIzaSyBzHzcHRupfXyaO6g8S50dr6eWnx4IBpUI',
+            language: 'en',
+          }}
+          suppressDefaultStyles
+          renderRow={GooglePlaceData => (
+            <SuggestionRow item={GooglePlaceData} />
+          )}
+        />
+      </View>
+
       <TextInput
         style={styles.textInput}
         placeholder="Where are you going?"
@@ -30,19 +41,6 @@ const LocationSearchScreen = props => {
         onChangeText={setInputText}></TextInput>
 
       {/* List of Destination */}
-      <FlatList
-        data={searchResults}
-        renderItem={({item}) => (
-          <Pressable
-            onPress={() => navigation.navigate('Guests')}
-            style={styles.row}>
-            <View style={styles.iconContainer}>
-              <Entypo name={'location-pin'} size={30} />
-            </View>
-            <Text style={styles.locationText}>{item.description}</Text>
-          </Pressable>
-        )}
-      />
     </View>
   );
 };
--- AirbnbClone/src/screens/LocationSearch/styles.js
+++ AirbnbClone/src/screens/LocationSearch/styles.js
@@ -5,7 +5,6 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   textInput: {
-    marginTop: 100,
     fontSize: 20,
     marginBottom: 20,
   },
