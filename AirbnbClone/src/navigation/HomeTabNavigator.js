46ecddc02447206e6dceef99815861a60ffcd838
Chiara Wahsono <chiarawahsono@gmail.com>
2021-06-29 17:27:15 +0700

    updated feed.js dummy data

--- AirbnbClone/src/navigation/HomeTabNavigator.js
+++ AirbnbClone/src/navigation/HomeTabNavigator.js
@@ -9,6 +9,8 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Feather from 'react-native-vector-icons/Feather';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import SearchResultsTabNavigator from './SearchResultsTabNavigator';
+import SearchResultsMap from '../screens/SearchResultsMap';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +19,8 @@ const HomeTabNavigator = () => {
     <Tab.Navigator tabBarOptions={{activeTintColor: '#f15454'}}>
       <Tab.Screen
         name={'Explore'}
-        component={ExploreNavigator}
+        // component={ExploreNavigator}
+        component={SearchResultsMap}
         options={{
           tabBarIcon: ({color}) => (
             <Fontisto name="search" size={25} color={color} />
--- AirbnbClone/src/screens/SearchResultsMap/index.js
+++ AirbnbClone/src/screens/SearchResultsMap/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Marker} from 'react-native-maps';
 
 const SearchResultsMap = props => {
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
         style={{width: '100%', height: '100%'}}
+        provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
-        }}
-      />
+        }}>
+        <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}}>
+          <View
+            style={{
+              backgroundColor: 'white',
+              padding: 5,
+              borderRadius: 10,
+              borderColor: 'grey',
+              borderWidth: 1,
+            }}>
+            <Text style={{fontWeight: 'bold'}}>$200</Text>
+          </View>
+        </Marker>
+      </MapView>
     </View>
   );
 };
