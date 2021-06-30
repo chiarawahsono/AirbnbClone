17e3bbe260aeec98cc13a5991b14afd059d149d4
Chiara Wahsono <chiarawahsono@gmail.com>
2021-06-30 15:47:01 +0700

    isSelected element embedded

--- AirbnbClone/src/screens/SearchResultsMap/index.js
+++ AirbnbClone/src/screens/SearchResultsMap/index.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {Marker} from 'react-native-maps';
+import CustomMarker from '../../components/CustomMarker';
+
+import places from '../../../assets/data/feed';
 
 const SearchResultsMap = props => {
+  const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
         style={{width: '100%', height: '100%'}}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: 28.3279822,
+          longitude: -16.5124847,
+          latitudeDelta: 0.8,
+          longitudeDelta: 0.8,
         }}>
-        <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}}>
-          <View
-            style={{
-              backgroundColor: 'white',
-              padding: 5,
-              borderRadius: 10,
-              borderColor: 'grey',
-              borderWidth: 1,
-            }}>
-            <Text style={{fontWeight: 'bold'}}>$200</Text>
-          </View>
-        </Marker>
+        {places.map(place => (
+          <CustomMarker
+            isSelected={place.id === selectedPlaceId}
+            onPress={() => setSelectedPlaceId(place.id)}
+            coordinate={place.coordinate}
+            price={place.newPrice}
+          />
+        ))}
       </MapView>
     </View>
   );
