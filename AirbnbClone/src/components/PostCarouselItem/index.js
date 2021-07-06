bd41af00599b39d948c646659aa86b5bec5b759f
Chiara Wahsono <chiarawahsono@gmail.com>
2021-07-06 14:43:08 +0700

    fixed bug with PostCarouselItem component

--- AirbnbClone/src/components/PostCarouselItem/index.js
+++ AirbnbClone/src/components/PostCarouselItem/index.js
@@ -1,38 +1,40 @@
 import React from 'react';
-import {View, Image, Text, Pressable} from 'react-native';
-import {useWindowDimensions} from 'react-native';
-import styles from './styles';
+import {View, Text, Image, useWindowDimensions, Pressable} from 'react-native';
+import styles from './styles.js';
 import {useNavigation} from '@react-navigation/native';
 
 const Post = props => {
   const post = props.post;
   const width = useWindowDimensions().width;
+
   const navigation = useNavigation();
-  const goToPostPage = navigation.navigate('Post', {postId: post.id});
+
+  const goToPostPage = () => {
+    navigation.navigate('Post', {postId: post.id});
+  };
 
   return (
     <Pressable
       onPress={goToPostPage}
       style={[styles.container, {width: width - 60}]}>
       <View style={styles.innerContainer}>
-        {/* Image */}
-        <Image
-          style={styles.image}
-          source={{
-            uri: post.image,
-          }}
-        />
+        {/* Image  */}
+        <Image style={styles.image} source={{uri: post.image}} />
+
         <View style={{flex: 1, marginHorizontal: 10}}>
+          {/* Bed & Bedroom  */}
           <Text style={styles.bedrooms}>
             {post.bed} bed {post.bedroom} bedroom
           </Text>
+
           {/* Type & Description */}
-          <Text style={styles.description}>
-            {post.type} {post.title}
+          <Text style={styles.description} numberOfLines={2}>
+            {post.type}. {post.title}
           </Text>
-          {/* Old price + New Price */}
+
+          {/*  Old price & new price */}
           <Text style={styles.prices}>
-            <Text style={styles.newPrice}>${post.newPrice} </Text>/ night
+            <Text style={styles.price}>${post.newPrice} </Text>/ night
           </Text>
         </View>
       </View>
