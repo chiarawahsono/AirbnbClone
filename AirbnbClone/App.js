015e921483ac8ebc466c6402407763a3e2eb8a9b
Chiara Wahsono <chiarawahsono@gmail.com>
2021-07-07 14:04:50 +0700

    amplify auth

--- AirbnbClone/App.js
+++ AirbnbClone/App.js
@@ -8,19 +8,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-// import HomeScreen from './src/screens/Home';
-// import Post from './src/components/Post';
-// import feed from './assets/data/feed';
-// import SearhResultsScreen from './src/screens/SearchResults';
-// import LocationSearchScreen from './src/screens/LocationSearch';
-// import GuestsScreen from './src/screens/Guests';
-// const post1 = feed[0];
-// const post2 = feed[1];
-// const post3 = feed[2];
+import {withAuthenticator} from 'aws-amplify-react-native';
+
 import Router from './src/navigation/router';
 
 const App = () => {
   return <Router />;
 };
-
-export default App;
+export default withAuthenticator(App);
