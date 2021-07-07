7c2fb0f8457a88adbb1b286e9c1120fc81084d14
Chiara Wahsono <chiarawahsono@gmail.com>
2021-07-07 15:01:51 +0700

    fixed amplify auth bug

--- AirbnbClone/index.js
+++ AirbnbClone/index.js
@@ -5,8 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+import Amplify, {Auth} from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 AppRegistry.registerComponent(appName, () => App);
