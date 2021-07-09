11833fe81e59a57ba3cf4640f35a1f8369184acb
Chiara Wahsono <chiarawahsono@gmail.com>
2021-07-09 13:58:54 +0700

    connected API backend

--- AirbnbClone/src/screens/SearchResults/index.js
+++ AirbnbClone/src/screens/SearchResults/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, FlatList} from 'react-native';
 import Post from '../../components/Post';
 import feed from '../../../assets/data/feed';
+import {listPosts} from '../../graphql/queries';
+import {API, graphqlOperation} from 'aws-amplify';
 
 const SearchResultsScreen = () => {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const postsResult = await API.graphql(graphqlOperation(listPosts));
+        console.log(postsResult);
+      } catch (e) {}
+    };
+  });
+
   return (
     <View>
       <FlatList data={feed} renderItem={({item}) => <Post post={item} />} />
